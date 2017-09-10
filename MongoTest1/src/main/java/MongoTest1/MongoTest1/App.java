10d513d6a6f11563dadcd17a710070eaf8a4c412
pradeepprasad <pradeepprasad1@gmail.com>
2017-09-10 23:13:40 +0100

    Initial Commit

---
+++ MongoTest1/src/main/java/MongoTest1/MongoTest1/App.java
@@ -0,0 +1,42 @@
+package MongoTest1.MongoTest1;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * Java + MongoDB  Example
+ *
+ */
+public class App {
+	public static void main(final String[] args) {
+		// Establish connection
+        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+        // get handle to "admin" database
+        MongoDatabase database = mongoClient.getDatabase("admin");
+
+        // get a handle to the "Customer" collection
+        MongoCollection<Document> collection = database.getCollection("Customer");
+
+        // Print the count
+               
+        System.out.println("Count of Collection - " + collection.count());
+
+        // Print all documents
+        System.out.println(" collection print starts here");
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+                
+            }
+        } finally {
+        	System.out.println("Collection print Ends here");
+            cursor.close();
+        }
+             
+}
+}
